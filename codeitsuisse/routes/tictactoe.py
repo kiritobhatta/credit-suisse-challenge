517ff15eb74f8a7ec89e39c6471b1a4bbd9d0f22
Tingweiftw <tingwei.f@gmail.com>
2021-09-25 15:28:18 +0800

    CHANGE TO post

--- codeitsuisse/routes/tictactoe.py
+++ codeitsuisse/routes/tictactoe.py
@@ -7,7 +7,7 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-@app.route('/tic-tac-toe', methods=['GET'])
+@app.route('/tic-tac-toe', methods=['POST'])
 def evaluateTTT():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
